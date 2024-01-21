67bee1b408c1ad2540ba80b867975e91b7fe3b08
oliverkinch <oliver.kinch@gmail.com>
2024-01-21 10:04:30 +0100

    New key

--- tests/processor/test_processor.py
+++ tests/processor/test_processor.py
@@ -18,7 +18,7 @@ def processed_data(config, processor):
 
 @pytest.mark.parametrize(
     "key",
-    ["case_id", "tabular_data", "pdf_data"],
+    ["case_id", "tabular_data", "pdf_data", "process_info"],
 )
 def test_tabular_data(processed_data, key):
     assert processed_data[key]
