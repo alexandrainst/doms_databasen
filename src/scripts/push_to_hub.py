2c8861727897443d8dd13e41c3e3fbe32f5197a6
oliverkinch <oliver.kinch@gmail.com>
2024-02-13 09:14:47 +0100

    Check dataset before push

--- src/scripts/push_to_hub.py
+++ src/scripts/push_to_hub.py
@@ -6,6 +6,7 @@ Usage:
 
 
 import logging
+from pathlib import Path
 
 import hydra
 from datasets import load_dataset
@@ -17,7 +18,18 @@ logger = logging.getLogger(__name__)
 @hydra.main(config_path="../../config", config_name="config")
 def main(config: DictConfig) -> None:
     dataset = load_dataset(config.paths.data_final_dir)
+
+    # Ensure that there is one sample in the
+    # dataset for each processed case
+    assert dataset.num_rows["train"] == _count_folders_in_dir(
+        Path(config.paths.data_processed_dir)
+    )
+
     dataset.push_to_hub(config.hf_hub, private=True)
+
+
+def _count_folders_in_dir(dir: Path) -> int:
+    return len([f for f in dir.iterdir() if f.is_dir()])
 
 
 if __name__ == "__main__":
